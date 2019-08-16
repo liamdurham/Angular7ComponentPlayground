8a7afbed99d8f2e950fc0e64edd1b875be4838e7
liamdurham <liam.durham@yahoo.com>
2019-08-16 00:57:07 -0400

    6 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from  '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import { LedgerBarComponent } from './ledger-bar/ledger-bar.component';
+import { NavComponent } from './nav/nav.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatIconModule ],
-  declarations: [ AppComponent, HelloComponent, SidebarComponent, LedgerBarComponent ],
+  imports:      [ BrowserModule, FormsModule, MatIconModule, RouterModule ],
+  declarations: [ AppComponent, HelloComponent, SidebarComponent, LedgerBarComponent, NavComponent, BannerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
